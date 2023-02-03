badb7b73523d8e6712ec1c56651b528eb32130a6
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 19:48:56 -0300

    Feature start route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import filedocx from './files/EditalDeAcessoARecursos2023.docx';
 import filepdf from './files/DiagnosticoSocioambientalParticipativo.pdf';
-import Routes from "./Routes/routes";
+// import Routes from "./Routes/routes";
+import Home from "./pages/Home";
 
 function App() {
 
   return (
-    <Routes />
+    <Home />
   );
 }
 
