5c99b7695b0662e59416738d796286dc2fdbb3fc
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 20:00:10 -0300

    Feature start route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import filedocx from './files/EditalDeAcessoARecursos2023.docx';
-import filepdf from './files/DiagnosticoSocioambientalParticipativo.pdf';
-// import Routes from "./Routes/routes";
-import Home from "./pages/Home";
 
 function App() {
 
   return (
-    <Home />
+    <div className="App">
+      <header className="App-header">
+        <p>
+          test
+        </p>
+      </header>
+    </div>
   );
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,32 @@
-// import { Link } from 'react-router-dom';
+// import React from "react";
+// import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-function Home() {
-   return (
-     <>
-       <p>Routes</p>
-     </>
- 
-   );
- }
- 
- export default Home;
- 
+// import Home from "../pages/Home";
+// // import Program from "../pages/Program";
+// // import Notice from "../Pages/Notice";
+// // import TheCommunities from "../Pages/TheCommunities";
+// // import Gallery from "../Pages/Gallery";
+// // import Directors from "../Pages/Directors";
+// // import YouInThePark from "../Pages/YouInThePark";
+// // import Team from "../Pages/Team";
+
+// const RoutesScreens = () => {
+//    return(
+//        <BrowserRouter basename={process.env.PUBLIC_URL}>
+//         <Routes>
+//            <Route path="/" element={<Home/>} exact />
+//          {/* <Route path="/site-favela" element={<Home/>} exact />
+//            <Route path="/o-programa" element={<Program />} /> */}
+//            {/*<Route path="/edital" element={<Notice />} />
+//            <Route path="/as-comunidades" element={<TheCommunities />} />
+//            <Route path="/galeria" element={<Gallery />} />
+//            <Route path="/realizadores" element={<Directors />} />
+//            <Route path="/vocenoparque" element={<YouInThePark />} />
+//            <Route path="/equipe" element={<Team />} /> */}
+//            {/* <Route path="/*" element={<Home/>} exact /> */}
+//         </Routes>
+//        </BrowserRouter>
+//    )
+// }
+
+// export default RoutesScreens;
