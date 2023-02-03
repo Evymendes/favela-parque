bcbe431c6db37415b32efd8e0d7b21b2de6a081f
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 19:33:53 -0300

    Feature start route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import filedocx from './files/EditalDeAcessoARecursos2023.docx';
 import filepdf from './files/DiagnosticoSocioambientalParticipativo.pdf';
-import Routes from "./routes/routes";
 
 function App() {
 
