32e1c613f523e4613d5748c9bacae965fd18ad89
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 18:08:00 -0300

    Feature start route

---
+++ src/pages/Program/index.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Program() {
+  return (
+    <>
+      <p>O programa</p>
+      <Link to='/'> Ir para tela Home</Link>
+    </>
+  );
+}
+
+export default Program;
+
