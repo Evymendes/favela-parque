bdf5bed8c0a3daacd2990585d17ef8a6e5df8f1a
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 20:19:34 -0300

    Feature start route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import Home from './pages/Home'
 function App() {
 
   return (
-        <Home />
+    <div className="App">
+      <Routes>
+        {/* Componente da tela inicial */}
+        <Route index element={<Home />} />
+        {/* Componente de uma rota específica */}
+        {/* <Route path="about" element={<About />} /> */}
+        {/* Componente para quando não encontrar uma rota */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </div>
   );
 }
 
