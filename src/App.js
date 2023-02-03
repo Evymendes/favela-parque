8711aa0729a83934529b19b2b8e4280cc4afa770
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 19:42:50 -0300

    Feature start route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 import filedocx from './files/EditalDeAcessoARecursos2023.docx';
 import filepdf from './files/DiagnosticoSocioambientalParticipativo.pdf';
-import Routes from "./routes/routes";
+import Routes from "./Routes/routes";
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          test
-        </p>
-       
-        <a href={filedocx} download>Click to download import docx</a>
-        <a href={filepdf} download>Click to download import import pdf</a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
