4406ecd5f82c869d70d6e7ac85f72fa6e9444569
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 20:02:22 -0300

    Feature start route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          test
-        </p>
+        <Home />
       </header>
     </div>
   );
