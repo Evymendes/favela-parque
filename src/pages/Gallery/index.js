41081592d64a8bccf06a2ce7ab8378b0d570e568
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 21:03:11 -0300

    Feature start route

---
+++ src/pages/Gallery/index.js
@@ -0,0 +1,18 @@
+import Menu from '../../components/Menu';
+
+// import * as S from './styles';
+
+function Gallery() {
+
+  return (
+    <div>
+      <Menu />
+      <p>Aqui nesta página você acompanhará o desenvolvimento visual do Programa a partir do seu lançamento.</p>
+      <p>Por enquanto, veja a preparação para o lançamento:</p>
+      <p>Em construção</p>
+    </div>
+  );
+}
+
+export default Gallery;
+
