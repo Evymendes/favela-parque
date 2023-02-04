947cc9f130f3f1900aee8820c3fd956466a2fb68
Evymendes <evelyn.laurindo@gmail.com>
2023-02-04 04:47:56 -0300

    uncommented route file

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,19 @@
-import React from "react";
-// import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-// import Home from "./pages/Home";
-// import Notice from "./pages/Notice"
+// import Login from '../pages/login';
+import Home from "./pages/Home";
 import Program from "./pages/Program";
-// import Notice from './pages/Notice';
-// import TheCommunities from "./pages/TheCommunities";
-// import Gallery from "./pages/Gallery";
-// import Directors from "./pages/Directors";
-// import YouInThePark from "./pages/YouInThePark";
-import Team from "./pages/Team";
 
-const RoutesScreens = () => {
-   return(
-      <Router>
-        <Routes>
-           {/* <Route path="/" element={<Home/>} exact /> */}
-           {/* <Route path="/" element={<Notice />} /> */}
-           <Route path="/o-programa" element={<Program />} />
-           <Route path="/equipe" element={<Team />} />
-           {/* <Route path="/edital" element={<Notice />} />
-           <Route path="/as-comunidades" element={<TheCommunities />} />
-           <Route path="/galeria" element={<Gallery />} />
-           <Route path="/realizadores" element={<Directors />} />
-           <Route path="/vocenoparque" element={<YouInThePark />} />
-           <Route path="/*" element={<Home/>} exact />  */}
-        </Routes>
-      </Router>
-   )
-}
+const Routes = (props) => {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/0-programa' component={Program} />
+			</Switch>
+		</BrowserRouter>
+	)
+};
 
-export default RoutesScreens;
+export default Routes;
