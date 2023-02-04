d55c6ea45e0db71d3805d5e16ab4de5696a5f8ff
Evymendes <evelyn.laurindo@gmail.com>
2023-02-04 04:41:53 -0300

    uncommented route file

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,34 @@
-// Libs
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from "react";
+// import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+// import Notice from "./pages/Notice"
 import Program from "./pages/Program";
+// import Notice from './pages/Notice';
+// import TheCommunities from "./pages/TheCommunities";
+// import Gallery from "./pages/Gallery";
+// import Directors from "./pages/Directors";
+// import YouInThePark from "./pages/YouInThePark";
+// import Team from "./pages/Team";
 
-const AppRoutes = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/o-programa" element={<Program />} />
+const RoutesScreens = () => {
+   return(
+      <Router>
+        <Routes>
+           {/* <Route path="/" element={<Home/>} exact /> */}
+           {/* <Route path="/" element={<Notice />} /> */}
+           <Route path="/o-programa" element={<Program />} />
+           {/* <Route path="/edital" element={<Notice />} />
+           <Route path="/as-comunidades" element={<TheCommunities />} />
+           <Route path="/galeria" element={<Gallery />} />
+           <Route path="/realizadores" element={<Directors />} />
+           <Route path="/vocenoparque" element={<YouInThePark />} />
+           <Route path="/equipe" element={<Team />} />
+           <Route path="/*" element={<Home/>} exact />  */}
+        </Routes>
+      </Router>
+   )
+}
 
-        {/* <UnauthenticatedRoute exact path='/' component={Home} />
-				<UnauthenticatedRoute exact path='/cadastro-da-organizacao' component={OngFormScreen} /> */}
-        {/* <PrivateRoute exact path='/config/precisa-de-ajuda' component={NeedHelp} /> */}
-      </Routes>
-    </Router>
-  )
-};
-
-export default AppRoutes;
+export default RoutesScreens;
