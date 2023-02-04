b82311a165b8da9ba01db277b96a7a7ddc535739
Evymendes <evelyn.laurindo@gmail.com>
2023-02-04 04:39:13 -0300

    _redirects file

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,23 @@
-import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+// Libs
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-// import Home from "./pages/Home";
-// import Notice from "./pages/Notice"
+import Home from "./pages/Home";
 import Program from "./pages/Program";
-// import Notice from './pages/Notice';
-// import TheCommunities from "./pages/TheCommunities";
-// import Gallery from "./pages/Gallery";
-// import Directors from "./pages/Directors";
-// import YouInThePark from "./pages/YouInThePark";
-// import Team from "./pages/Team";
 
-const RoutesScreens = () => {
-   return(
-      <BrowserRouter >
-        <Routes>
-           {/* <Route path="/" element={<Home/>} exact /> */}
-           {/* <Route path="/" element={<Notice />} /> */}
-           <Route path="/o-programa" element={<Program />} />
-           {/* <Route path="/edital" element={<Notice />} />
-           <Route path="/as-comunidades" element={<TheCommunities />} />
-           <Route path="/galeria" element={<Gallery />} />
-           <Route path="/realizadores" element={<Directors />} />
-           <Route path="/vocenoparque" element={<YouInThePark />} />
-           <Route path="/equipe" element={<Team />} />
-           <Route path="/*" element={<Home/>} exact />  */}
-        </Routes>
-      </BrowserRouter>
-   )
-}
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/o-programa" element={<Program />} />
 
-export default RoutesScreens;
+        {/* <UnauthenticatedRoute exact path='/' component={Home} />
+				<UnauthenticatedRoute exact path='/cadastro-da-organizacao' component={OngFormScreen} /> */}
+        {/* <PrivateRoute exact path='/config/precisa-de-ajuda' component={NeedHelp} /> */}
+      </Routes>
+    </Router>
+  )
+};
+
+export default AppRoutes;
