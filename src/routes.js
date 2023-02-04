47729904d69a08783e0e827cf94e207715c5985f
Evymendes <evelyn.laurindo@gmail.com>
2023-02-04 03:38:37 -0300

    uncommented team file

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 // import Home from "./pages/Home";
+import Home from "./pages/Home"
 import Program from "./pages/Program";
 // import Notice from './pages/Notice';
 // import TheCommunities from "./pages/TheCommunities";
@@ -15,6 +16,7 @@ const RoutesScreens = () => {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
            {/* <Route path="/" element={<Home/>} exact /> */}
+           <Route path="/" element={<Home />} />
            <Route path="/o-programa" element={<Program />} />
            {/* <Route path="/edital" element={<Notice />} />
            <Route path="/as-comunidades" element={<TheCommunities />} />
