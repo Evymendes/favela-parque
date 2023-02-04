1c9788dceb3f83a17e5b478ff61fe101e4d84a9d
Evymendes <evelyn.laurindo@gmail.com>
2023-02-04 03:14:09 -0300

    uncommented home file

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,33 +15,7 @@ import {
 
     return (
       <S.Container>
-        <Menu />
-        <S.Main>
-          <S.Background>
-            <S.ContentText>
-             {children}
-            </S.ContentText>
-            </S.Background>
-          <S.Section>
-            <S.ContentLogo>
-              <S.ImgLogo src={Logo} alt='Logo'  />
-              <S.LogoText>Programa de Educação Socioambiental do Parque Nacional da Tijuca</S.LogoText>
-            </S.ContentLogo>
-            <S.BackgBlack />
-            <S.Footer>
-              <div>
-                <S.TextAchievement>Realização</S.TextAchievement>
-                <S.ContentPartnerships>
-                    <S.ImgPartnerships src={Corcovado} alt='Logo Corcovado'  />
-                    <S.ImgPartnerships src={PNTijuca} alt='Logo PNTijuca'  />
-                    <S.ImgICM src={ICMBio} alt='Logo ICMBio'  />
-                    <S.ImgPartnerships src={PrecisaSer} alt='Logo PrecisaSer'  />
-                </S.ContentPartnerships>
-              </div>
-              <S.ContentImageFav />
-            </S.Footer>
-          </S.Section>
-        </S.Main>
+        <p>Home</p>
       </S.Container>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Program from "./pages/Program";
-// import Notice from './pages/Notice';
-// import TheCommunities from "./pages/TheCommunities";
-// import Gallery from "./pages/Gallery";
-// import Directors from "./pages/Directors";
-// import YouInThePark from "./pages/YouInThePark";
-// import Team from "./pages/Team";
+import Notice from './pages/Notice';
+import TheCommunities from "./pages/TheCommunities";
+import Gallery from "./pages/Gallery";
+import Directors from "./pages/Directors";
+import YouInThePark from "./pages/YouInThePark";
+import Team from "./pages/Team";
 
 const RoutesScreens = () => {
    return(
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-           {/* <Route path="/" element={<Home/>} exact /> */}
+           <Route path="/" element={<Home/>} exact />
            <Route path="/o-programa" element={<Program />} />
-           {/* <Route path="/edital" element={<Notice />} />
+           <Route path="/edital" element={<Notice />} />
            <Route path="/as-comunidades" element={<TheCommunities />} />
            <Route path="/galeria" element={<Gallery />} />
            <Route path="/realizadores" element={<Directors />} />
            <Route path="/vocenoparque" element={<YouInThePark />} />
            <Route path="/equipe" element={<Team />} />
-           <Route path="/*" element={<Home/>} exact />  */}
+           <Route path="/*" element={<Home/>} exact /> 
         </Routes>
       </BrowserRouter>
    )
