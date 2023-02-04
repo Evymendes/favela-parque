9ef2c67fad5e0c83668caed99d5d1c14b78de869
Evymendes <evelyn.laurindo@gmail.com>
2023-02-03 22:06:38 -0300

    Hotfix delete files

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Program from "./pages/Program";
-import Notice from './pages/Notice';
-import TheCommunities from "./pages/TheCommunities";
-import Gallery from "./pages/Gallery";
-import Directors from "./pages/Directors";
-import YouInThePark from "./pages/YouInThePark";
-import Team from "./pages/Team";
+// import Notice from './pages/Notice';
+// import TheCommunities from "./pages/TheCommunities";
+// import Gallery from "./pages/Gallery";
+// import Directors from "./pages/Directors";
+// import YouInThePark from "./pages/YouInThePark";
+// import Team from "./pages/Team";
 
 const RoutesScreens = () => {
    return(
