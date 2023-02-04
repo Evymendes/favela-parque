570cc3e0c284c966b86aea411953fd81ec2e2ba4
Evymendes <evelyn.laurindo@gmail.com>
2023-02-04 03:27:08 -0300

    uncommented home file

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Program from "./pages/Program";
-// import Notice from './pages/Notice';
-// import TheCommunities from "./pages/TheCommunities";
-// import Gallery from "./pages/Gallery";
-// import Directors from "./pages/Directors";
-// import YouInThePark from "./pages/YouInThePark";
-// import Team from "./pages/Team";
+import Notice from "./pages/Program";
+import TheCommunities from "./pages/TheCommunities";
+import Gallery from "./pages/Gallery";
+import Directors from "./pages/Directors";
+import YouInThePark from "./pages/YouInThePark";
+import Team from "./pages/Team";
 
 const RoutesScreens = () => {
    return(
